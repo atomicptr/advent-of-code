981b87a00efdd0ac9dceb8d57cc1316de524c47d
Christopher Kaster <me@atomicptr.de>
2023-12-04 22:48:44 +0900

    add solution for day 4 part 2, albeit really slow

--- 2023/04/main.rs
+++ 2023/04/main.rs
@@ -1,10 +1,12 @@
 use crate::ParseError::InvalidFormat;
+use std::collections::HashMap;
 use std::str::FromStr;
 
 fn main() {
     let input = include_str!("./input.txt");
     let game = Game::from_str(input).expect("should parse");
     println!("Part 1) Result: {}", game.total_ppints());
+    println!("Part 2) Result: {}", game.scratch_cards_total());
 }
 
 #[derive(Debug)]
@@ -13,12 +15,48 @@ enum ParseError {
 }
 
 struct Game {
-    cards: Vec<Card>,
+    cards: HashMap<u32, Card>,
 }
 
 impl Game {
     fn total_ppints(&self) -> u32 {
-        self.cards.iter().map(|card| card.points()).sum()
+        self.cards.values().map(|card| card.points()).sum()
+    }
+
+    fn scratch_cards(&self) -> HashMap<u32, u32> {
+        let mut cards = HashMap::new();
+
+        let mut scratch_cards: Vec<u32> = self.cards.values().map(|card| card.id).collect();
+
+        let mut index = 0;
+
+        while index < scratch_cards.len() {
+            let card_id: u32 = scratch_cards.get(index).unwrap().clone();
+            scratch_cards.extend(self.scratch_card_ids_for(card_id.clone()));
+
+            let val = match cards.get(&card_id) {
+                Some(val) => val,
+                None => &0_u32,
+            };
+
+            cards.insert(card_id, val + 1);
+            index += 1;
+        }
+
+        cards
+    }
+
+    fn scratch_cards_total(&self) -> u32 {
+        self.scratch_cards().values().sum()
+    }
+
+    fn scratch_card_ids_for(&self, id: u32) -> Vec<u32> {
+        if let Some(card) = self.cards.get(&id) {
+            let count = card.user_winning_numbers().len();
+            return (1..=count).map(|num| id + (num as u32)).collect();
+        }
+
+        vec![]
     }
 }
 
@@ -26,16 +64,16 @@ impl FromStr for Game {
     type Err = ParseError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        Ok(Game {
-            cards: s
-                .trim()
-                .lines()
-                .map(|line| match Card::from_str(line) {
-                    Err(_) => panic!("could not parse card"),
-                    Ok(card) => card,
-                })
-                .collect(),
-        })
+        let mut cards = HashMap::new();
+
+        for line in s.trim().lines() {
+            match Card::from_str(line) {
+                Ok(card) => cards.insert(card.id, card),
+                Err(err) => return Err(err),
+            };
+        }
+
+        Ok(Game { cards })
     }
 }
 
@@ -118,6 +156,7 @@ impl FromStr for Card {
 #[cfg(test)]
 mod test_2023_04 {
     use crate::Game;
+    use std::collections::HashMap;
     use std::str::FromStr;
 
     const TEST_EXAMPLE: &str = "\
@@ -132,13 +171,38 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
     fn test_parse_example() {
         let game = Game::from_str(TEST_EXAMPLE).expect("should exist");
 
-        let test_points = [8, 2, 2, 1, 0, 0];
-        let test_total_points: u32 = test_points.iter().sum();
+        let test_points = HashMap::from([(1, 8), (2, 2), (3, 2), (4, 1), (5, 0), (6, 0)]);
+        let test_total_points: u32 = test_points.values().sum();
 
-        for (index, card) in game.cards.iter().enumerate() {
-            assert_eq!(test_points.get(index).unwrap().clone(), card.points());
+        for (id, card) in game.cards.iter() {
+            assert_eq!(test_points.get(id).unwrap().clone(), card.points());
         }
 
         assert_eq!(test_total_points, game.total_ppints());
+    }
+
+    const TEST_EXAMPLE_PART_2: &str = "\
+Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+
+    #[test]
+    fn test_parse_example_part2() {
+        let game = Game::from_str(TEST_EXAMPLE_PART_2).expect("should exist");
+
+        let scratch_card_counts = game.scratch_cards();
+
+        let expected_counts = HashMap::from([(1, 1), (2, 2), (3, 4), (4, 8), (5, 14), (6, 1)]);
+        let expected_total = 30;
+
+        for (id, num) in scratch_card_counts.iter() {
+            let expected = expected_counts.get(id).unwrap();
+            assert_eq!(expected, num, "ID: {id} expected: {expected} but got {num}");
+        }
+
+        assert_eq!(expected_total, game.scratch_cards_total());
     }
 }
